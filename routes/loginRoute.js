48d0547daa7dbd3514879ad110084840cc4c3c0b
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-08-13 13:59:03 +0200

    wip: login

---
+++ routes/loginRoute.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const loginRouter = express.Router();
+
+loginRouter.post("/login", (req, res) => {
+  try {
+    console.log(req.body);
+    // If ok, redirect to home page/dashboard
+    res.status(200).json({ msg: "Login Ok" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
+});
+
+export default loginRouter;
