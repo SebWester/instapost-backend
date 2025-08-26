17578cf121e7d3d1bdb422d9ba5811913817c1d5
Vicci35 <toja.1999@hotmail.com>
2025-08-26 15:33:30 +0200

    route for update profil

---
+++ routes/updateProfil.js
@@ -0,0 +1,43 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import User from "../models/User.js";
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+const router = express.Router();
+
+router.post("/update-profile", async (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Ingen token hittades" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const userId = decoded.id;
+    const { name, bio, profilePic } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        name,
+        bio,
+        profileImage: profilePic,
+      },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "Användare hittades inte" });
+    }
+
+    res.json({ success: true, user: updatedUser });
+  } catch (err) {
+    console.error("JWT/DB error:", err);
+    res.status(401).json({ error: "Ogiltlig token eller fel i databasen" });
+  }
+});
+
+export default router;
