d0ff9c1161b92c94043536d857b2d1caeb2c1d9d
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-08-27 14:44:16 +0200

    Refactor login and logout flows for better performance and faster user experience. Begin implementing photo capture feature for creating new posts.

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -11,54 +11,25 @@ const token_key = process.env.JWT_SECRET_KEY;
 
 loginRouter.post("/login", async (req, res) => {
   const { email, password, platform } = req.body;
-
-  const user = await User.findOne({ email: email });
-  console.log(user);
-  if (user === null) {
-    res.status(404).json({ error: "Wrong username" });
-  }
+  const user = await User.findOne({ email });
+  if (!user) return res.status(404).json({ error: "Wrong username" });
 
   const passwordOk = await compare(password, user.password);
-  if (!passwordOk) {
-    res.status(401).json({ error: "Wrong password" });
+  if (!passwordOk) return res.status(401).json({ error: "Wrong password" });
+
+  const token = jwt.sign({ name: user.name }, token_key, { expiresIn: "1h" });
+
+  const responsePayload = { token, userData: user };
+
+  if (platform === "web") {
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
   }
 
-  const token = jwt.sign(
-    {
-      name: user.name,
-    },
-    token_key,
-    { expiresIn: "1h" }
-  );
-
-  try {
-    if (platform === "web") {
-      // Change later
-      res.cookie("token", token, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "lax",
-      });
-      res.status(200).json({ token: token });
-    } else {
-      console.log("Welcome");
-      res.status(200).json({ token: token });
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err });
-  }
-});
-
-loginRouter.post("/login/userdata", async (req, res) => {
-  const { email } = req.body;
-
-  const user = await User.findOne({ email: email });
-  if (user === null) {
-    res.status(404).json({ error: "Wrong username" });
-  }
-
-  res.status(200).json({ userData: user });
+  res.status(200).json(responsePayload);
 });
 
 export default loginRouter;
