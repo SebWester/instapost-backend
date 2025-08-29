d51f850229125b81702cd52bd2d8ea81f7f21c2c
Vicci35 <toja.1999@hotmail.com>
2025-08-29 18:01:25 +0200

    fix: add ES module __dirname support for static uploads

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
