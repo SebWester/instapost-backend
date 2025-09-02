18b6bcccc4b8d52fb1f0831ffd5b8d2ddbb21704
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-09-02 14:56:21 +0200

    wip: new post

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,6 +1,21 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
 
 const postRouter = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // spara här
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const randomName = Date.now() + "-" + Math.round(Math.random() * 1e9) + ext;
+    cb(null, randomName);
+  },
+});
+
+const upload = multer({ storage });
 
 postRouter.get("/", (req, res) => {
   try {
@@ -11,6 +26,18 @@ postRouter.get("/", (req, res) => {
 });
 
 // New post
-// postRouter.post("/new");
+postRouter.post("/new", upload.single("image"), (req, res) => {
+  const { text, userId, username } = req.body;
+  const image = req.file;
+  console.log(req.body);
+  try {
+    res.json({
+      message: "Post saved",
+      data: { text, userId, username, image },
+    });
+  } catch (err) {
+    console.error("I HAVE ESCAPED FROM THE POLICE!", err);
+  }
+});
 
 export default postRouter;
